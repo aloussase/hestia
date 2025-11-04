fb6cc960ea7759731c3aa14388dbcd212ac4c095
Alexander Goussas <alexandergoussas.thoughtworks@latam.com>
2025-11-03 22:33:42 -0500

    feat: add panel to display output of system command
    Closes #4

--- src/main/java/io/github/aloussase/hestia/config/PanelConfig.java
+++ src/main/java/io/github/aloussase/hestia/config/PanelConfig.java
@@ -1,12 +1,16 @@
 package io.github.aloussase.hestia.config;
 
+import io.github.aloussase.hestia.panel.CmdPanel;
 import io.github.aloussase.hestia.panel.DockerPanel;
 import io.github.aloussase.hestia.panel.IPanel;
 import io.github.aloussase.hestia.panel.PostgresPanel;
+import lombok.Data;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,13 +21,24 @@ public class PanelConfig {
 
     private final List<IPanel> panels = new ArrayList<>();
 
+    @ConfigurationProperties(prefix = "dashboard.panels")
+    @Component
+    @Data
+    public static class CmdPanelsConfig {
+        private List<String> cmds;
+    }
+
     public PanelConfig(
             @Value("${dashboard.panels.docker.enabled}")
             boolean dockerPanelEnabled,
             @Value("${dashboard.panels.postgres.enabled}")
             boolean postgresPanelEnabled,
+            CmdPanelsConfig cmdPanelsConfig,
             ApplicationContext applicationContext
-    ) {
+    ) throws Exception {
+        for (final var cmd : cmdPanelsConfig.getCmds()) {
+            panels.add(CmdPanel.class.getDeclaredConstructor(String.class).newInstance(cmd));
+        }
         if (dockerPanelEnabled)
             panels.add(applicationContext.getBean(DockerPanel.class));
         if (postgresPanelEnabled)
